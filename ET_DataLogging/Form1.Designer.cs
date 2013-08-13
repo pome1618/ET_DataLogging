4f4b217fc4970a6020d48195864d272dc3a3c44f
pome1618 <pome1618@gmail.com>
2013-08-13 22:53:28 +0900

    Ver 1.00

---
+++ ET_DataLogging/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ET_DataLogging
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(236, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "connect";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(789, 15);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "set";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(76, 175);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Run";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(547, 175);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Stop";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(994, 175);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "Start";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(76, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 20);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "time",
+            "light",
+            "gyro",
+            "motor_R",
+            "motor_L"});
+			this.comboBox2.Location = new System.Drawing.Point(628, 15);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 20);
+			this.comboBox2.TabIndex = 6;
+			// 
+			// chart1
+			// 
+			chartArea3.AxisX.Interval = 5000D;
+			chartArea3.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+			chartArea3.AxisX.Title = "time";
+			chartArea3.AxisY.Interval = 100D;
+			chartArea3.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+			chartArea3.AxisY.Title = "light/gyro";
+			chartArea3.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea3);
+			legend3.Name = "Legend1";
+			this.chart1.Legends.Add(legend3);
+			this.chart1.Location = new System.Drawing.Point(12, 204);
+			this.chart1.Name = "chart1";
+			series5.ChartArea = "ChartArea1";
+			series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series5.Legend = "Legend1";
+			series5.Name = "light";
+			series6.ChartArea = "ChartArea1";
+			series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series6.Legend = "Legend1";
+			series6.Name = "gyro";
+			this.chart1.Series.Add(series5);
+			this.chart1.Series.Add(series6);
+			this.chart1.Size = new System.Drawing.Size(1160, 370);
+			this.chart1.TabIndex = 7;
+			this.chart1.Text = "chart1";
+			// 
+			// chart2
+			// 
+			chartArea4.AxisX.Interval = 5000D;
+			chartArea4.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+			chartArea4.AxisX.Title = "motor";
+			chartArea4.AxisY.Interval = 5000D;
+			chartArea4.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+			chartArea4.AxisY.Title = "time";
+			chartArea4.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea4);
+			legend4.Name = "Legend1";
+			this.chart2.Legends.Add(legend4);
+			this.chart2.Location = new System.Drawing.Point(12, 580);
+			this.chart2.Name = "chart2";
+			series7.ChartArea = "ChartArea1";
+			series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series7.Legend = "Legend1";
+			series7.Name = "motor_R";
+			series8.ChartArea = "ChartArea1";
+			series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series8.Legend = "Legend1";
+			series8.Name = "motor_L";
+			this.chart2.Series.Add(series7);
+			this.chart2.Series.Add(series8);
+			this.chart2.Size = new System.Drawing.Size(1160, 370);
+			this.chart2.TabIndex = 8;
+			this.chart2.Text = "chart2";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("HGP明朝E", 100F);
+			this.label1.Location = new System.Drawing.Point(421, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(337, 134);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "0000";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1184, 961);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.chart2);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "DataLogger";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ ET_DataLogging/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ET_DataLogging
+{
+	public partial class Form1 : Form
+	{
+		//ポート関係の宣言
+		SerialPort myPort;
+		static string PortName;
+		int BaudRate;
+		Parity Parity;
+		int DataBits;
+		StopBits StopBits;
+
+		//表示する値のコマンド
+		int cmd;
+
+		//読み込むbufferr
+		string buf = "";
+		string buf2 = "";
+		int find = 0;
+
+		//csv用のファイル
+		System.IO.StreamWriter fs;
+
+		//各変数の宣言
+		int time = 0, light = 0, gyro = 0, motor_R = 0, motor_L = 0;
+
+		//Thread
+		System.Threading.Thread Datalogging_Thread;
+		delegate void MyDelegate(string target);
+
+		delegate void graph_del();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			//グラフの初期化
+			chart1.Series[0].Points.Clear();
+			chart1.Series[1].Points.Clear();
+			chart2.Series[0].Points.Clear();
+			chart2.Series[1].Points.Clear();
+
+			//折れ線グラフ
+			chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+			chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+			chart2.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+			chart2.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+
+			//グラフの線の設定
+			chart1.Series[0].Color = Color.Red;
+			chart1.Series[1].Color = Color.Green;
+			chart2.Series[0].Color = Color.Blue;
+			chart2.Series[1].Color = Color.Yellow;
+
+			//軸の調整
+			chart1.ChartAreas[0].AxisX.Maximum = 1000;
+			chart1.ChartAreas[0].AxisX.Minimum = 0;
+			chart1.ChartAreas[0].AxisY.Maximum = 700;
+			chart1.ChartAreas[0].AxisY.Minimum = 400;
+			chart2.ChartAreas[0].AxisX.Maximum = 1000;
+			chart2.ChartAreas[0].AxisX.Minimum = 0;
+			chart2.ChartAreas[0].AxisY.Maximum = 500;
+			chart2.ChartAreas[0].AxisY.Minimum = 0;
+
+			//ポートの設定
+			BaudRate = 38400;
+			Parity = Parity.None;
+			DataBits = 8;
+			StopBits = StopBits.One;
+
+			//接続可能なCOMポートを検出、コンボボックスに入れる
+			string[] portNames = SerialPort.GetPortNames();
+
+			// 取得したシリアル・ポート名をコンボボックスにセット
+			comboBox1.Items.Clear();
+			foreach (string port in portNames)
+			{
+				this.comboBox1.Items.Add(port);
+			}
+
+			// コンボボックスのデフォルト選択をセット
+			this.comboBox1.SelectedIndex = 0;
+			this.comboBox2.SelectedIndex = 0;
+
+			//ファイルを作成
+			string FileName = System.DateTime.Now.ToString();	//時間を取得
+			FileName = FileName.Replace('/', '_');				// 2013/07/13を2013_07_13という形に整形
+			FileName = FileName.Replace(":", "_");				//18:23:30を18_23_30という形に整形
+			FileName = FileName.Replace(" ", "_");				//␣を＿に置き換え
+			FileName += ".csv";
+			//MyDocumentsにJ2_logがあったらそこに保存
+			if (System.IO.Directory.Exists(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\J2_log"))
+			{
+				fs = new System.IO.StreamWriter(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\J2_log\\" + FileName);
+			}
+			else
+			{
+				fs = new System.IO.StreamWriter(FileName);
+			}
+			MessageBox.Show(FileName, "ファイル作成完了");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//コンボボックスからCOMポート名を取得
+			//取得したCOMポート名でポートを開く
+			PortName = comboBox1.Text;
+			if (myPort == null && PortName.Length >= 4 && PortName.Length != 0)
+			{
+				//ポートがnullでポート名が大丈夫そうならポートを開く
+				myPort = new SerialPort(PortName, BaudRate, Parity, DataBits, StopBits);
+				try
+				{
+					myPort.Open();
+				}
+				catch
+				{
+					MessageBox.Show("error", "error");
+				}
+			}
+			if (myPort != null && myPort.IsOpen)
+			{
+				//接続に成功したらメッセージを出す
+				MessageBox.Show("接続成功");
+				//接続に成功したらコンボボックスをロックする
+				comboBox1.Enabled = false;
+			}
+			else
+			{
+				MessageBox.Show("error", "error");
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//値(id)を取得
+			switch (comboBox2.SelectedIndex)
+			{
+				case 0: cmd = 0;
+					break;
+				case 1: cmd = 1;
+					break;
+				case 2: cmd = 2;
+					break;
+				case 3: cmd = 3;
+					break;
+				case 4: cmd = 4;
+					break;
+				default: MessageBox.Show("error", "error");
+					break;
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			//'R'を送信する
+			if (myPort != null && myPort.IsOpen)
+			{
+				myPort.Write("R");
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			//'S'を送信する
+			if (myPort != null && myPort.IsOpen)
+			{
+				myPort.Write("S");
+				myPort.Close();
+			}
+			/*
+						//目盛の調整(凍結)
+						chart1.ChartAreas[0].AxisY.Minimum -= chart1.ChartAreas[0].AxisY.Minimum % 100;
+						chart1.ChartAreas[0].AxisY.Maximum += 100 - chart1.ChartAreas[0].AxisY.Maximum % 100;
+						chart2.ChartAreas[0].AxisY.Maximum += 100 - chart2.ChartAreas[0].AxisY.Maximum % 100;
+						chart1.ChartAreas[0].AxisX.Minimum = 0;
+						chart2.ChartAreas[0].AxisX.Minimum = 0;
+						chart2.ChartAreas[0].AxisY.Minimum = 0;
+			*/
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			this.Datalogging_Thread =
+			new Thread(new ThreadStart(this.datalogging));
+			this.Datalogging_Thread.Start();
+		}
+
+		public void datalogging()
+		{
+			if (myPort != null && myPort.IsOpen)
+			{
+				//無限ループ
+				while (true)
+				{
+					if (myPort.IsOpen == false)
+					{
+						break;
+					}
+					//bufferの取得
+					try
+					{
+						buf = myPort.ReadLine();
+					}
+					catch
+					{
+						MessageBox.Show("error", "error");
+					}
+					//NULL文字を消す
+					buf = buf.TrimStart('\0');
+
+					//誤作動予防
+					if (buf.Length > 30 || buf.Length < 5)
+					{
+						continue;
+					}
+					if (buf.Length > 30 || buf.Length < 5)
+					{
+						continue;
+					}
+
+					//CSVへの書き込み
+					fs.WriteLine(buf);
+
+					//各変数の取得
+
+					//timeの値取得
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out time) == false)
+					{
+						continue;
+					}
+					if (cmd == 0)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(time) });
+					}
+
+					//lightの値取得
+					buf = buf.Remove(0, find + 1);
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out light) == false)
+					{
+						continue;
+					}
+					if (cmd == 1)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(light) });
+					}
+
+					//gyroの値取得
+					buf = buf.Remove(0, find + 1);
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out gyro) == false)
+					{
+						continue;
+					}
+					gyro = Convert.ToInt32(buf2);
+					if (cmd == 2)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(gyro) });
+					}
+
+					//moter_Rの値取得
+					buf = buf.Remove(0, find + 1);
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out motor_R) == false)
+					{
+						continue;
+					}
+					if (cmd == 3)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(motor_R) });
+					}
+					//moter_Lの値取得
+					buf = buf.Remove(0, find + 1);
+					buf2 = buf;
+					if (int.TryParse(buf2, out motor_L) == false)
+					{
+						continue;
+					}
+					if (cmd == 4)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(motor_L) });
+					}
+					if (!(time > 100000 || time < 0 || gyro > 1000 || light > 1000 || gyro < 0 || light < 0))
+					{
+						graph();
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("error", "error");
+			}
+		}
+		void SetText(string target)
+		{
+			label1.Text = target;
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			//フォームが閉じるときにポートも閉じる
+			if (myPort != null && myPort.IsOpen)
+			{
+				myPort.Close();
+			}
+		}
+
+		void graph()
+		{
+			//スレッドセーフ
+			if (this.InvokeRequired)
+			{
+				graph_del d = new graph_del(graph);
+				this.Invoke(d, new object[] { });
+			}
+			else
+			{
+				//軸調整
+				if (time > chart1.ChartAreas[0].AxisX.Maximum)
+				{
+					chart1.ChartAreas[0].AxisX.Maximum = time;
+					chart2.ChartAreas[0].AxisX.Maximum = time;
+				}
+				if (time < chart1.ChartAreas[0].AxisX.Minimum)
+				{
+					chart1.ChartAreas[0].AxisX.Minimum = time;
+					chart2.ChartAreas[0].AxisX.Minimum = time;
+				}
+				if (light > chart1.ChartAreas[0].AxisY.Maximum)
+				{
+					chart1.ChartAreas[0].AxisY.Maximum = light;
+				}
+				if (light < chart1.ChartAreas[0].AxisY.Minimum)
+				{
+					chart1.ChartAreas[0].AxisY.Minimum = light;
+				}
+				if (gyro > chart1.ChartAreas[0].AxisY.Maximum)
+				{
+					chart1.ChartAreas[0].AxisY.Maximum = gyro;
+				}
+				if (gyro < chart1.ChartAreas[0].AxisY.Minimum)
+				{
+					chart1.ChartAreas[0].AxisY.Minimum = gyro;
+				}
+
+				if (motor_R > chart2.ChartAreas[0].AxisY.Maximum)
+				{
+					chart2.ChartAreas[0].AxisY.Maximum = motor_R;
+				}
+				if (motor_R < chart2.ChartAreas[0].AxisY.Minimum)
+				{
+					chart2.ChartAreas[0].AxisY.Minimum = motor_R;
+				}
+				if (motor_L > chart2.ChartAreas[0].AxisY.Maximum)
+				{
+					chart2.ChartAreas[0].AxisY.Maximum = motor_L;
+				}
+				if (motor_L < chart2.ChartAreas[0].AxisY.Minimum)
+				{
+					chart2.ChartAreas[0].AxisY.Minimum = motor_L;
+				}
+
+				//プロット
+				chart1.Series[0].Points.AddXY(time, light);
+				chart1.Series[1].Points.AddXY(time, gyro);
+				chart2.Series[0].Points.AddXY(time, motor_R);
+				chart2.Series[1].Points.AddXY(time, motor_L);
+			}
+
+		}
+
+		//データをチェックする
+		public bool check_data(String str)
+		{
+			bool flag = true;
+			foreach (char c in str)
+			{
+				//数字以外の文字が含まれているか調べる
+				if (c != '-' && (c < '0' || '9' < c))
+				{
+					flag = false;
+					break;
+				}
+			}
+			if (str.Length >= 10)
+			{
+				flag = false;
+			}
+			return flag;
+		}
+
+	}
+}
